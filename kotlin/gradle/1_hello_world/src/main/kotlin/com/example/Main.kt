26b6fa63503b848990331061f08f7492e5a443e1
FromSi <fromsi665@gmail.com>
2018-12-15 15:41:30 +0600

    Added kotlin gradle hello_world

---
+++ kotlin/gradle/1_hello_world/src/main/kotlin/com/example/Main.kt
@@ -0,0 +1,18 @@
+package com.example
+
+import spark.kotlin.*
+
+/**
+ * Метод для запуска WebSocket
+ */
+fun main(args: Array<String>) {
+
+    /**
+     * Запрос GET /hello
+     * Доступен по ссылке http://localhost:4567/hello
+     * или по IP http://127.0.0.1:4567/hello
+     */
+    get("/hello") {
+        "Hello World"
+    }
+}
